8fb5e4e6764b3a7c2dfa4b39dae7472085299386
Iain Watt <iainskwatt@gmail.com>
2021-07-13 20:03:56 +0100

    Just commit to make sure all is saved. Not got anywhere!

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 let buttonClick = document.querySelectorAll("button");
 let display = document.getElementById("output");
 let displayValue = "";
-
-
+let leftNum = 0;
+let rightNum = 0;
+let nums = ['0','1','2','3','4','5','6','7','8','9'];
+let symbols = ['+', '-', 'x', '/'];
 
 //functions for basic math operations
 function add (a, b) {
@@ -37,17 +39,28 @@ function operate (operator, a, b) {
             calc = divide(a, b);
             break;
     }
-    return calc;
+    display.textContent = '';
+    updateDisplay(calc);
 }
 
 function updateDisplay (id) {
-    display.textContent = id;
+    display.textContent += id;
+    displayValue = display.textContent;
+    console.log(displayValue);
 }
 
 function getButtonClicks (){
-    buttonClick.forEach(element => {
-        element.addEventListener('click', (e) => {
-            updateDisplay(element.id);
+    buttonClick.forEach(button => {
+        button.addEventListener('click', (e) => {
+            if (nums.includes(button.id)){
+                updateDisplay(button.id);
+            } else if (symbols.includes(button.id)) {
+                leftNum = Number(displayValue);
+                op = button.id;
+            } else if (button.id == '=') {
+                operate(op, leftNum, rightNum);
+            }
+            
         });
     });
 }
